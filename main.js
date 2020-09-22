cec9a0456c2ce385531df208c3aab9ede1cbe79f
1204136013 <yami1204136013@outlook.com>
2020-09-23 02:03:06 +0800

    拆分 utils

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-var log = console.log.bind(console)
-
-var imageFromPath = function (path) {
-    var img = new Image()
-    img.src = path
-    return img
-}
 
 var loadLevel = function (n) {
     blocks = []
--- utils.js
+++ utils.js
@@ -1,12 +1,12 @@
 var recIntersects = function (a, b) {
-    // var o = a
-    // if (b.y > o.y && b.y < o.y + o.image.height) {
-    //     if (b.x > o.x && b.x < o.x + o.image.width) {
-    //         log("相撞")
-    //         return true
-    //     }
-    // }
-    // return false
     no = a.y + a.image.height < b.y || a.x + a.image.width < b.x || b.y + b.image.height < a.y || b.x + b.image.width < a.x
     return !no
+}
+
+var log = console.log.bind(console)
+
+var imageFromPath = function (path) {
+    var img = new Image()
+    img.src = path
+    return img
 }
