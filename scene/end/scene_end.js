6becdb10f3748b3973af58b5d82b4290c21ff6b9
1204136013 <yami1204136013@outlook.com>
2020-10-10 00:32:50 +0800

    重启游戏场景

--- scene/end/scene_end.js
+++ scene/end/scene_end.js
@@ -2,6 +2,12 @@ var SceneEnd = function(game) {
     var s = {
         game: game,
     }
+
+    game.registerAction("r", function () {
+        var s  = SceneTitle(game)
+        game.replaceScene(s)
+        })
+
     s.draw = function() {
         // Create gradient
         var gradient=game.context.createLinearGradient(0,0,400,0);
@@ -10,7 +16,7 @@ var SceneEnd = function(game) {
         gradient.addColorStop("1.0","red");
 
         game.context.fillStyle=gradient
-        game.context.fillText("Game Over", 100, 30)
+        game.context.fillText("Game Over, press r to restart", 100, 30)
 
     }
     s.update = function() {
