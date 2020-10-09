cf6b2fce6f974f2b3f2eed3c17522aaf08d3d851
1204136013 <yami1204136013@outlook.com>
2020-10-10 01:00:39 +0800

    Update four_scene.js

--- game/four_scene.js
+++ game/four_scene.js
@@ -9,29 +9,3 @@ class FourScene {
 
     }   
 }
-
-
-class SceneTitle extends FourScene {
-    constructor(game) {
-        super(game)
-        game.registerAction("k", function () {
-            var s  = Scene(game)
-            game.replaceScene(s)
-            })
-    }
-
-    draw() {
-        var gradient = this.game.context.createLinearGradient(0,0,400,0);
-        gradient.addColorStop("0","magenta");
-        gradient.addColorStop("0.5","blue");
-        gradient.addColorStop("1.0","red");
-
-        this.game.context.fillStyle=gradient
-        this.game.context.fillText("开始游戏, press k", 100, 30)
-
-    }
-    update() {
-
-    }   
-
-}
