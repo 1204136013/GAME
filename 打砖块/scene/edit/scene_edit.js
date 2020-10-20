41d8abc8d15b072e65a7c22429144128e30073dc
1204136013 <yami1204136013@outlook.com>
2020-10-20 11:54:59 +0800

    Revert "Update scene_edit.js"
    This reverts commit 74d74f57a6ec2514bb6cf6e789c5a758b774bac7.

--- 打砖块/scene/edit/scene_edit.js
+++ 打砖块/scene/edit/scene_edit.js
@@ -2,35 +2,34 @@ class SceneEdit extends FourScene {
     constructor(game) {
         super(game)
         game.registerAction("k", function () {
-            var s = Scene(game)
+            var s  = Scene(game)
             game.replaceScene(s)
-        })
-        this.blocks = []
-        game.canvas.addEventListener("mousedown", (event) => {
+            })
+        this.block = Block(game, [0, 0])
+        game.canvas.addEventListener("mouseup", (event) => {
             var x = event.offsetX
             var y = event.offsetY
             log(x, y, "up")
             log("this", this)
             log("thisblock", this.block)
-            this.blocks.push(Block(game, [x, y]))
+            this.block.x = x
+            this.block.y = y
         })
+        
     }
 
     draw() {
-        var gradient = this.game.context.createLinearGradient(0, 0, 400, 0);
-        gradient.addColorStop("0", "magenta")
-        gradient.addColorStop("0.5", "blue")
-        gradient.addColorStop("1.0", "red")
-        this.game.context.fillStyle = gradient
+        var gradient = this.game.context.createLinearGradient(0,0,400,0);
+        gradient.addColorStop("0","magenta")
+        gradient.addColorStop("0.5","blue")
+        gradient.addColorStop("1.0","red")
+        this.game.context.fillStyle=gradient
         this.game.context.fillText("关卡编辑", 100, 30)
-        for (var i = 0; i < this.blocks.length; i++) {
-            var block = this.blocks[i]
-            this.game.drawImage(block)
-        }
+        this.game.drawImage(this.block)
     }
     update() {
-
-    }
+        
+    } 
 
 
 
