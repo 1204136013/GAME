cd6b8a3c3fe14ac65376101ba2322aeb6ee479b3
1204136013 <yami1204136013@outlook.com>
2020-11-20 20:35:32 +0800

    Update score.js

--- flappy_bird/scene/main/score.js
+++ flappy_bird/scene/main/score.js
@@ -1,6 +1,7 @@
 class Score {
     constructor(game) {
         this.game = game
+        this.score = 0
     }
 
     static new(game) {
@@ -8,38 +9,19 @@ class Score {
     }
 
     update() {
-        for (var p of this.pipes) {
-            p.x -= 5
-            if (p.x < -100) {
-                p.x += this.管子横向间距 * this.columsOfPipe
-            }
-        }
     }
 
     draw() {
-        var context = this.game.context
-        for (var p of this.pipes) {
-            context.save()
+    }
 
-            var w2 = p.w / 2
-            var h2 = p.h / 2
-
-            context.translate(p.x + w2, p.y + h2)
-            var scaleX = p.flipX ? -1 : 1
-            var scaleY = p.flipY ? -1 : 1
-            context.scale(scaleX, scaleY)
-            context.rotate(p.rotation * Math.PI / 180)
-
-
-            context.translate(-w2, -h2)
-
-            context.drawImage(p.texture, 0, 0)
-            context.restore()
+    // 将数字的各个数位分离
+    parseNumber(n) {
+        n = "" + n
+        var r  = []
+        for (x of n) {
+            r.push(x)
         }
+        return r
     }
 
-    resetPipesPostion(p1, p2) {
-        p1.y = randomBetween(-200, 0)
-        p2.y = p1.y + p1.h + this.pipeSpace
-    }
 }
