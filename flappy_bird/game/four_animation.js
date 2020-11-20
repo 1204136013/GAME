074f0cb44a0b134bd709da6bd8022714012ddd7d
1204136013 <yami1204136013@outlook.com>
2020-11-20 20:49:38 +0800

    Update four_animation.js

--- flappy_bird/game/four_animation.js
+++ flappy_bird/game/four_animation.js
@@ -62,19 +62,14 @@ class FourAnimation {
     draw() {
         var context = this.game.context
         context.save()
-
         var w2 = this.w / 2
         var h2 = this.h / 2
-
         context.translate(this.x + w2, this.y + h2)
         if (this.flipX) {
             context.scale(-1, 1)
         }
         context.rotate(this.rotation * Math.PI / 180)
-
-
         context.translate(-w2, -h2)
-
         context.drawImage(this.texture, 0, 0)
         context.restore()
         // this.game.drawImage(this)
