0ccac1b52c8ea2e35f5951574ff574aecc055f2b
LAPTOP-K74S93SF\Yami1 <yami1204136013@outlook.com>
2020-12-19 17:50:20 +0800

    bird 去除 编辑场景

--- flappy_bird/scene/edit/scene_edit.js
+++ flappy_bird/scene/edit/scene_edit.js
@@ -1,36 +1,3 @@
 class SceneEdit extends FourScene {
-    constructor(game) {
-        super(game)
-        game.registerAction("k", function () {
-            var s  = Scene(game)
-            game.replaceScene(s)
-            })
-        this.block = Block(game, [0, 0])
-        game.canvas.addEventListener("mouseup", (event) => {
-            var x = event.offsetX
-            var y = event.offsetY
-            log(x, y, "up")
-            log("this", this)
-            log("thisblock", this.block)
-            this.block.x = x
-            this.block.y = y
-        })
-        
-    }
-
-    draw() {
-        var gradient = this.game.context.createLinearGradient(0,0,400,0);
-        gradient.addColorStop("0","magenta")
-        gradient.addColorStop("0.5","blue")
-        gradient.addColorStop("1.0","red")
-        this.game.context.fillStyle=gradient
-        this.game.context.fillText("关卡编辑", 100, 30)
-        this.game.drawImage(this.block)
-    }
-    update() {
-        
-    } 
-
-
 
 }
